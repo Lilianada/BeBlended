56e1511789d532a58229cd6d8763cdbcdee9cf72
Lilianada <lilibest30.lb@gmail.com>
2022-10-29 06:18:37 -0400

    ...

--- src/components/StylistBookingPage/Appointment/AppointmentSection.jsx
+++ src/components/StylistBookingPage/Appointment/AppointmentSection.jsx
@@ -47,13 +47,8 @@ function AppointmentSection() {
         <option value="Day" defaultValue={true}>
           Day
         </option>
-        <option value="Monday">Monday</option>
-        <option value="Tuesday">Tuesday</option>
-        <option value="Wednesday">Wednesday</option>
-        <option value="Thursday">Thursday</option>
-        <option value="Friday">Friday</option>
-        <option value="Saturday">Saturday</option>
-        <option value="Sunday">Sunday</option>
+        <option value="Week">Week</option>
+        <option value="Month">Month</option>
       </select>
     </section>
   );
