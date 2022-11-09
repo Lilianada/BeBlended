ca0c976bd26ef4169200c9d9aae8be9a601a4828
Lilianada <lilibest30.lb@gmail.com>
2022-11-09 09:29:00 -0500

    price range modal

--- src/components/ListingPage/ListingPage.jsx
+++ src/components/ListingPage/ListingPage.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
-import { IoIosPricetags, IoMdTime }from 'react-icons/io';
-import { IoLocationSharp }from 'react-icons/io5';
+import { IoIosPricetags, IoMdTime } from "react-icons/io";
+import { IoLocationSharp } from "react-icons/io5";
 import { BottomNavClient, Footer } from "../../components";
 import ListingHead from "./ListingHead/ListingHead";
-import './ListingPage.scss';
+import "./ListingPage.scss";
 import Listings from "./Listings";
+import PriceRangeModal from "./PriceRangeModal/PriceRangeModal";
 
-export default function ListingPage () {
+export default function ListingPage() {
   const [openModal, setOpenModal] = useState({
     price: false,
     available: false,
     distance: false,
-  })
+  });
   const handleModal = (price, available, distance) => {
     setOpenModal((prevState) => {
       return {
@@ -24,33 +25,73 @@ export default function ListingPage () {
   };
   return (
     <section className="mainWrapper">
-      <ListingHead/>
+      <ListingHead />
       <div className="listingTitle">
         <h4 className="title">32 Search Results in Toronto</h4>
         <div className="titleButtons">
-          <button className="btnPrimary" onClick={()=> handleModal('price')} >
-            <IoIosPricetags size={18} className='icon' />
+          <button className="btnPrimary" onClick={() => handleModal("price")}>
+            <IoIosPricetags size={18} className="icon" />
             Price
           </button>
-          <button className="btnPrimary" onClick={()=> handleModal('available')} >
-            <IoMdTime size={18} className='icon' />
+          <AnimatePresence
+            initial={false}
+            exitBeforeEnter={true}
+            onExitComplete={() => null}
+          >
+            {openModal.price && (
+              <PriceRangeModal
+                openModal={open.price}
+                closeModal={setOpenModal}
+              />
+            )}
+          </AnimatePresence>
+          <button
+            className="btnPrimary"
+            onClick={() => handleModal("available")}
+          >
+            <IoMdTime size={18} className="icon" />
             Available Now
           </button>
-          <button className="btnPrimary" onClick={()=> handleModal('distance')} >
-            <IoLocationSharp size={18} className='icon' />
+          <AnimatePresence
+            initial={false}
+            exitBeforeEnter={true}
+            onExitComplete={() => null}
+          >
+            {openModal.available && (
+              <PriceRangeModal
+                openModal={open.available}
+                closeModal={setOpenModal}
+              />
+            )}
+          </AnimatePresence>
+          <button
+            className="btnPrimary"
+            onClick={() => handleModal("distance")}
+          >
+            <IoLocationSharp size={18} className="icon" />
             Distance
           </button>
+          <AnimatePresence
+            initial={false}
+            exitBeforeEnter={true}
+            onExitComplete={() => null}
+          >
+            {openModal.distance && (
+              <PriceRangeModal
+                openModal={open.distance}
+                closeModal={setOpenModal}
+              />
+            )}
+          </AnimatePresence>
         </div>
       </div>
       <div className="listingFlex">
-        <Listings/>
-        <div className="map">
-          {/* Fix in the map here */}
-        </div>
+        <Listings />
+        <div className="map">{/* Fix in the map here */}</div>
       </div>
-      <BottomNavClient/>
+      <BottomNavClient />
       <div className="footer">
-        <Footer/>
+        <Footer />
       </div>
     </section>
   );
--- src/components/ListingPage/PriceRangeModal/PriceRangeModal.jsx
+++ src/components/ListingPage/PriceRangeModal/PriceRangeModal.jsx
@@ -1,28 +1,68 @@
 import React from "react";
-import {Backdrop} from "../../../components";
+import { Backdrop } from "../../../components";
 import { motion } from "framer-motion";
 import PriceRange from "../../../assets/PriceRange.svg";
-import './PriceRange.scss';
+import "./PriceRangeModal.scss";
+import { AiOutlineMinus } from "react-icons/ai";
+import CurrencyInput from "react-currency-input-field";
 
-export default function PriceRangeModal() {
-  return (
-    <section className="priceRangeModal">
-        <Backdrop>
+export default function PriceRangeModal({ openModal, closeModal }) {
+  return ReactDOM.createPortal(
+    <>
+      {openModal ? (
+        <section className="priceRangeModal">
+          <Backdrop onClick={closeModal}>
             <motion.div
-                className="dialogContent"
-                onClick={(e) => e.stopPropagation()}
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{
-                    duration: 0.8,
-                    delay: 0.5,
-                    ease: [0, 0.71, 0.2, 1.01],
-                }}
-                exit="exit"
+              className="dialogContent"
+              onClick={(e) => e.stopPropagation()}
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
+              }}
+              exit="exit"
             >
+              <div className="modalContent">
                 <img src={PriceRange} alt="Price Range" />
+                <div className="inputWrap">
+                  <label htmlFor="min-price" className="label">
+                    Min Price
+                    <CurrencyInput
+                      prefix="$"
+                      type="text|num"
+                      name="min-price"
+                      className="inputField"
+                      placeholder="00.00"
+                      groupSeparator=""
+                      disableGroupSeparators="true"
+                    />
+                  </label>
+                  <AiOutlineMinus size={18} />
+                  <label htmlFor="max-price" className="label">
+                    Max Price
+                    <CurrencyInput
+                      prefix="$"
+                      type="text|num"
+                      name="max-price"
+                      className="inputField"
+                      placeholder="00.00"
+                      groupSeparator=""
+                      disableGroupSeparators="true"
+                    />
+                  </label>
+                </div>
+                <div className="buttonWrap">
+                  <button className="clearBtn">clear</button>
+                  <button className="saveBtn">Save</button>
+                </div>
+              </div>
             </motion.div>
-        </Backdrop>
-    </section>
+          </Backdrop>
+        </section>
+      ) : null}
+    </>,
+    document.getElementById("modal")
   );
 }
