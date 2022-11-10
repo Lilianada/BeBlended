6507ab113cd45d561468dc76c2c5c0b2c9470c97
Lilianada <lilibest30.lb@gmail.com>
2022-11-10 09:59:36 -0500

    listing card

--- src/components/ListingPage/ListingPage.jsx
+++ src/components/ListingPage/ListingPage.jsx
@@ -1,9 +1,11 @@
 import { AnimatePresence } from "framer-motion";
 import React, { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 import { IoIosPricetags, IoMdTime } from "react-icons/io";
 import { IoLocationSharp } from "react-icons/io5";
 import { BottomNavClient, Footer } from "../../components";
 import DistanceModal from "./DistanceModal/DistanceModal";
+import ListingCard from "./ListingCard";
 import ListingHead from "./ListingHead/ListingHead";
 import "./ListingPage.scss";
 import Listings from "./Listings";
@@ -14,27 +16,31 @@ export default function ListingPage() {
     price: false,
     available: false,
     distance: false,
+    list: false,
   });
 
-  const handleModal = (price, distance, available) => {
+  const handleModal = (price, distance, available, list) => {
     setOpenModal((prevState) => {
       return {
         ...prevState,
         [price]: !prevState[price],
         [available]: !prevState[available],
         [distance]: !prevState[distance],
+        [list]: !prevState[list],
       };
     });
   };
 
-  
-  return(
-    <section className="mainWrapper">
+  return (
+    <section className="mainWrapper listingPage">
       <ListingHead />
       <div className="listingTitle">
         <h4 className="title">32 Search Results in Toronto</h4>
         <div className="titleButtons">
-          <button className={`btnPrimary ${openModal.price ? 'activeBtn' : ''}`} onClick={() => handleModal("price")}>
+          <button
+            className={`btnPrimary ${openModal.price ? "activeBtn" : ""}`}
+            onClick={() => handleModal("price")}
+          >
             <IoIosPricetags size={18} className="icon" />
             Price
           </button>
@@ -52,15 +58,15 @@ export default function ListingPage() {
           </AnimatePresence>
 
           <button
-            className={`btnPrimary ${openModal.available ? 'activeBtn' : ''}`}
+            className={`btnPrimary ${openModal.available ? "activeBtn" : ""}`}
             onClick={() => handleModal("available")}
           >
             <IoMdTime size={18} className="icon" />
             Available Now
           </button>
-          
+
           <button
-            className={`btnPrimary ${openModal.distance ? 'activeBtn' : ''}`}
+            className={`btnPrimary ${openModal.distance ? "activeBtn" : ""}`}
             onClick={() => handleModal("distance")}
           >
             <IoLocationSharp size={18} className="icon" />
@@ -80,10 +86,18 @@ export default function ListingPage() {
           </AnimatePresence>
         </div>
       </div>
+       
       <div className="listingFlex">
         <Listings />
         <div className="map">{/* Fix in the map here */}</div>
       </div>
+
+        {
+          openModal.list && (
+            <ListingCard openModal={openModal.list} closeModal={setOpenModal} />
+          )
+        }
+
       <BottomNavClient />
       <div className="footer">
         <Footer />
--- src/components/ListingPage/Listings.jsx
+++ src/components/ListingPage/Listings.jsx
@@ -1,43 +1,55 @@
-import React from "react";
-import { AiFillHeart, AiFillStar, AiOutlineHeart } from "react-icons/ai";
+import React, { useState } from "react";
+import {
+  AiFillHeart,
+  AiFillStar,
+  AiOutlineHeart,
+} from "react-icons/ai";
 import { ListingData } from "./ListingData";
 
 export default function Listings() {
   return (
-      <div className="listings">
-            {
-                ListingData.map((item, id) => (
-                <div className="listing" key={id}>
-                    <div className="listingsWrap" >
-                        <div className="profileImage">
-                            <img src={item.profileImage} alt="Listing Image" />
-                        </div>
-                        <div className="listingInfo">
-                            <h5 className="title"> {item.stylistName} </h5>
-                            <div className="ratingReviews">
-                                <AiFillStar size={18} style={{marginRight: '.75rem'}} fill="#893583" />
-                                <p className="rating"> {item.rating} </p>
-                                <p className="review"> ({item.reviews} reviews) </p>
-                            </div>
-                            <p className="location"> {item.address} </p>
-                            <div className="tags">
-                                {item.tags.map((tag, id) => (
-                                    <p className="tag" key={id}> {tag} </p>
-                                ))}
-                            </div>
-                            <div className="services">
-                                <p className="serviceTitle">Services provided:</p>
-                                <p className="serviceLists"> {item.services} </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="like">
-                        {item.like === true ? <AiFillHeart size={24} /> : <AiOutlineHeart size={24}/>}
-                    </div>
-                </div>
-                ))
-            }
-
+    <div className="listings">
+      {ListingData.map((item, id) => (
+        <div className="listing" key={id}>
+          <div className="listingsWrap">
+            <div className="profileImage">
+              <img src={item.profileImage} alt="Listing Image" />
+            </div>
+            <div className="listingInfo">
+              <h5 className="title"> {item.stylistName} </h5>
+              <div className="ratingReviews">
+                <AiFillStar
+                  size={18}
+                  style={{ marginRight: ".75rem" }}
+                  fill="#893583"
+                />
+                <p className="rating"> {item.rating} </p>
+                <p className="review"> ({item.reviews} reviews) </p>
+              </div>
+              <p className="location"> {item.address} </p>
+              <div className="tags">
+                {item.tags.map((tag, id) => (
+                  <p className="tag" key={id}>
+                    {" "}
+                    {tag}{" "}
+                  </p>
+                ))}
+              </div>
+              <div className="services">
+                <p className="serviceTitle">Services provided:</p>
+                <p className="serviceLists"> {item.services} </p>
+              </div>
+            </div>
+          </div>
+          <div className="like">
+            {item.like === true ? (
+              <AiFillHeart size={24} />
+            ) : (
+              <AiOutlineHeart size={24} />
+            )}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
