149cdac1b1660d06d9b212c22aab5f3ae96ef30f
Lilianada <lilibest30.lb@gmail.com>
2022-11-18 00:01:56 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ function App() {
       {/* <ClientProfile/>  */}
       {/* <StylistProfile/> */}
       
-    <BookingPage/>
-    {/* <ManageBookings/> */}
+    {/* <BookingPage/> */}
+    <ManageBookings/>
     
     {/* <ListingPage/> */}
     </div>
