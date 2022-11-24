21ae192499e7de177dda1746d97607b938dc33f8
Lilianada <lilibest30.lb@gmail.com>
2022-11-24 07:57:45 -0500

    ...

--- src/components/StylistAdmin/StylistAdminHome/StylistAdminHome.jsx
+++ src/components/StylistAdmin/StylistAdminHome/StylistAdminHome.jsx
@@ -1,24 +1,44 @@
 import React from "react";
-import './StylistAdminHome.scss';
+import "./StylistAdminHome.scss";
 import StylistAdminHeader from "../StylistAdminHeader/StylistAdminHeader";
+import Personal from "../../../assets/Admin-Icons/Personal-icon.svg";
+import Business from "../../../assets/Admin-Icons/Business-icon.svg";
+import { GrNext } from "react-icons/gr";
 
 export default function StylistAdminHome() {
-    
   return (
     <section className="mainWrapper stylistAdmin_Home">
-       <StylistAdminHeader/>
+      <StylistAdminHeader />
 
-        <div className="adminHead_Text">
-            <h5>Your Account</h5>
-            <p className="stylistName">
-               <strong>Braids by Becky</strong> - Becky Jones  </p>
-        </div>
+      <div className="adminHead_Text">
+        <h5>Your Account</h5>
+        <p className="stylistName">
+          <strong>Braids by Becky</strong> - Becky Jones{" "}
+        </p>
+      </div>
 
-        <div className="adminMenu_items">
-          <div className="menuItem">
-
+      <div className="adminMenu_items">
+        <div className="menuItem">
+          <img src={Personal} alt="Menu Icon" className="menuIcon" />
+          <div className="menuName">
+            <p>Personal Information</p>
+            <GrNext fontWeight={700} />
           </div>
+          <p className="menuInfo">
+            Provide personal details and let us know how we can reach you.
+          </p>
         </div>
+        <div className="menuItem">
+          <img src={Business} alt="Menu Icon" className="menuIcon" />
+          <div className="menuName">
+            <p>Business Profile</p>
+            <GrNext fontWeight={700} />
+          </div>
+          <p className="menuInfo">
+            Update your services, hours and all business information.
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
