4bdedb6724ffc8ff0d03e0cf554e4c3ef08ee338
Lilianada <lilibest30.lb@gmail.com>
2022-11-24 16:32:14 -0500

    Menu items

--- src/components/StylistAdmin/StylistAdminHome/StylistAdminHome.jsx
+++ src/components/StylistAdmin/StylistAdminHome/StylistAdminHome.jsx
@@ -3,7 +3,11 @@ import "./StylistAdminHome.scss";
 import StylistAdminHeader from "../StylistAdminHeader/StylistAdminHeader";
 import Personal from "../../../assets/Admin-Icons/Personal-icon.svg";
 import Business from "../../../assets/Admin-Icons/Business-icon.svg";
+import Finances from "../../../assets/Admin-Icons/Finances-icon.svg";
+import Clients from "../../../assets/Admin-Icons/Clients-icon.svg";
 import { GrNext } from "react-icons/gr";
+import { IoCalendarClearOutline, IoCard } from "react-icons/io5";
+import { MdOutlineSecurity } from "react-icons/md";
 
 export default function StylistAdminHome() {
   return (
@@ -22,20 +26,76 @@ export default function StylistAdminHome() {
           <img src={Personal} alt="Menu Icon" className="menuIcon" />
           <div className="menuName">
             <p>Personal Information</p>
-            <GrNext fontWeight={700} />
+            <GrNext fontWeight={700} size={14} />
           </div>
           <p className="menuInfo">
             Provide personal details and let us know how we can reach you.
           </p>
         </div>
+
         <div className="menuItem">
           <img src={Business} alt="Menu Icon" className="menuIcon" />
           <div className="menuName">
             <p>Business Profile</p>
-            <GrNext fontWeight={700} />
+            <GrNext fontWeight={700} size={14} />
           </div>
           <p className="menuInfo">
             Update your services, hours and all business information.
+          </p>
+        </div>
+
+        <div className="menuItem">
+          <img src={Finances} alt="Menu Icon" className="menuIcon" />
+          <div className="menuName">
+            <p>Finances</p>
+            <GrNext fontWeight={700} size={14} />
+          </div>
+          <p className="menuInfo">
+            View your past, current, and upcoming earnings.
+          </p>
+        </div>
+
+        <div className="menuItem">
+          <img src={Clients} alt="Menu Icon" className="menuIcon" />
+          <div className="menuName">
+            <p>Clients</p>
+            <GrNext fontWeight={700} size={14} />
+          </div>
+          <p className="menuInfo">
+            View, manage, and engage with your clientele.
+          </p>
+        </div>
+
+        <div className="menuItem">
+          <IoCalendarClearOutline
+            className="menuIcon"
+            size={24}
+            stroke="#893583"
+          />
+          <div className="menuName">
+            <p>Calendar</p>
+            <GrNext fontWeight={700} size={14} />
+          </div>
+          <p className="menuInfo">View, manage, and create appointments.</p>
+        </div>
+
+        <div className="menuItem">
+          <IoCard size={24} fill="#893583"  className="menuIcon"  />
+          <div className="menuName">
+            <p>Payout Information</p>
+            <GrNext fontWeight={700} size={14} />
+          </div>
+          <p className="menuInfo">Add and manage your payout information.</p>
+        </div>
+
+        <div className="menuItem">
+          <MdOutlineSecurity size={24} fill="893583"   className="menuIcon" />
+          <div className="menuName">
+            <p>Login & Security</p>
+            <GrNext fontWeight={700} size={14} />
+          </div>
+          <p className="menuInfo">
+            Update your password and secure your account.
           </p>
         </div>
       </div>
