d5d0c296c31475069ee85902d1dae2cae5753c93
Lilianada <lilibest30.lb@gmail.com>
2022-11-25 18:17:52 -0500

    loginSecurity

---
+++ src/components/StylistAdmin/LoginSecurity/LoginSecurity.jsx
@@ -0,0 +1,34 @@
+import { AnimatePresence } from "framer-motion";
+import React, { useState } from "react";
+import { GrNext, GrPrevious } from "react-icons/gr";
+import {
+  StylistAdminHeader,
+  BottomNavStylist
+} from "../../../components";
+
+export default function LoginSecurity() {
+    const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <main className="mainWrapper loginSecurity_page">
+      <StylistAdminHeader />
+
+      <div className="wd_80">
+        <div className="loginSecurity_head">
+          <GrPrevious style={{ marginRight: ".5rem" }} />
+          <p>Back</p>
+        </div>
+
+        <strong className="loginSecurity_subhead">
+          Account
+          <GrNext style={{ margin: " 0 .5rem" }} />
+          Login & Security
+        </strong>
+
+        <h4 className="formHead">Login & Security</h4>
+
+        </div>
+        <BottomNavStylist />
+    </main>
+  );
+}
