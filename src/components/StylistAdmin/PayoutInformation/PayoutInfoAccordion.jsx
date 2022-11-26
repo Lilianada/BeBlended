ca851190592a3e9f6c26ad9dac005808d8686b20
Lilianada <lilibest30.lb@gmail.com>
2022-11-26 18:19:32 -0500

    ...

--- src/components/StylistAdmin/PayoutInformation/PayoutInfoAccordion.jsx
+++ src/components/StylistAdmin/PayoutInformation/PayoutInfoAccordion.jsx
@@ -31,6 +31,7 @@ export default function PayoutInfoAccordion({body, head}) {
               duration: 0.8,
               ease: [0.04, 0.62, 0.23, 0.98],
             }}
+            style={{marginRight: "2rem"}}
           >
             {isActive ? (
               <GrDown className="accordionIcon" />
