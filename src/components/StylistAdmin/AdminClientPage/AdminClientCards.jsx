43b204d17b22b38f4108360e56f634929545031b
Lilianada <lilibest30.lb@gmail.com>
2022-11-28 11:39:44 -0500

    Admin Client Data and Cards

---
+++ src/components/StylistAdmin/AdminClientPage/AdminClientCards.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AdminClientCards() {
+  return <div>AdminClientCards</div>;
+}
---
+++ src/components/StylistAdmin/AdminClientPage/AdminClientData.js
@@ -0,0 +1,49 @@
+export const clientList = [
+    {
+        
+            id: 1,
+            clientImage: "https://images.unsplash.com/photo-1663431512656-b1d9e811735b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1899&q=80",
+            clientName: "Melly Thomas"  
+        },
+          {
+            id: 2,
+            clientImage: "https://images.unsplash.com/photo-1657130711047-522ce815a2c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+            clientName: "Jenny H."
+        },
+          {
+            id: 3,
+            clientImage: "https://images.unsplash.com/photo-1662859009178-62afb0cff049?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+            clientName: "Adjua Johnson"
+        },
+          {
+            id: 4,
+            clientImage: "https://images.unsplash.com/photo-1663431512297-993006b0098b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+            clientName: "Wura Makande"
+        },
+          {
+            id: 5,
+            clientImage: "https://images.unsplash.com/photo-1663387147575-49598f178640?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+            clientName: "Jada Weston"
+        },
+          {
+            id: 6,
+            clientImage: "https://images.unsplash.com/photo-1662695088711-acfdfda51c01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+            clientName: "Kelly Mane"
+        },
+          {
+            id: 7,
+            clientImage: "https://images.unsplash.com/photo-1663402989770-4c5bd9254896?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2N3x8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+            clientName: "Kemi Lewis"
+        },
+          {
+            id: 8,
+            clientImage: "https://images.unsplash.com/photo-1663383460653-bef91222019c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4Nnx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+            clientName: "Nkechi Okoro"
+        },
+          {
+            id: 9,
+            clientImage: "https://images.unsplash.com/photo-1662859010103-61b5735968ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMTJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+            clientName: "Adjua James"
+        },
+    
+]
