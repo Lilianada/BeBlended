09ff8f6489e3c3ebe12d5616ecfa47f1ba72d928
Lilianada <lilibest30.lb@gmail.com>
2022-11-29 09:26:45 -0500

    ...

--- src/components/StylistAdmin/AdminClientPage/AdminClientProfile/AdminClientProfile.jsx
+++ src/components/StylistAdmin/AdminClientPage/AdminClientProfile/AdminClientProfile.jsx
@@ -5,7 +5,7 @@ import { StylistAdminHeader } from "../../../../components";
 import BeblendedHead from "../../../../assets/Admin-Icons/Beblended-head.svg";
 import Kinky from "../../../../assets/KinkyHair.png";
 import Coarse from "../../../../assets/CoarseHair.png";
-import './AdminClientProfile.scss';
+import "./AdminClientProfile.scss";
 import { BsChat } from "react-icons/bs";
 
 export default function AdminClientProfile() {
@@ -23,44 +23,59 @@ export default function AdminClientProfile() {
         <div className="adminClient_subhead">
           <div className="subHead">
             <h4 className="formHead">
-              My Clients <AiOutlineMinus style={{margin: "0 .75rem"}} /> Melly Thomas
+              My Clients <AiOutlineMinus style={{ margin: "0 .75rem" }} /> Melly
+              Thomas
             </h4>
           </div>
         </div>
-        <hr/>
+        <hr />
       </div>
 
       <div className="clientProfile_Grid">
         <div className="clientDetails">
-            <img src={BeblendedHead} alt="Client Profile Image" className="clientImg"/>
-            <p className="clientName"> Melly Thomas </p>
-            <button className="chat">
-              <BsChat/>
-              Chat with Melly
-            </button>
-            <button className="add">
-              Add New Appointment
-            </button>
-            <div className="hairTexture">
-                <div className="texture" >
-                    <div className="textureCircle">
-                        <img src={Kinky} alt="Hair Texture" />
-                    </div>
-                    <h5 className="hairName"> Hair Texture </h5>
-                    <p className="textureName"> Kinky/Coily </p>
+          <img
+            src={BeblendedHead}
+            alt="Client Profile Image"
+            className="clientImg"
+          />
+          <p className="clientName"> Melly Thomas </p>
+          <button className="chat">
+            <BsChat />
+            Chat with Melly
+          </button>
+          <button className="add">Add New Appointment</button>
+          <div className="hairTexture">
+            <div className="texture">
+              <div className="textureCircle">
+                <img src={Kinky} alt="Hair Texture" />
+              </div>
+              <h5 className="hairName"> Hair Texture </h5>
+              <p className="textureName"> Kinky/Coily </p>
+            </div>
+            <div className="texture">
+              <div className="textureCircle">
+                <img src={Coarse} alt="Hair Structure" />
+              </div>
+              <h5 className="hairName"> Hair Structure </h5>
+              <p className="textureName"> Coarse </p>
+            </div>
+          </div>
+        </div>
+        <div className="clientAppointments">
+            <div className="flexHead">
+                <div className="flexItem">
+                    <h3>6</h3>
+                    <p>All Bookings</p>
                 </div>
-
-                <div className="texture">
-                    <div className="textureCircle">
-                    <img src={Coarse} alt="Hair Structure" />
-
-                    </div>
-                    <h5 className="hairName"> Hair Structure </h5>
-                    <p className="textureName"> Coarse </p>
+                <div className="flexItem">
+                    <h3>3</h3>
+                    <p>Completed</p>
+                </div>
+                <div className="flexItem">
+                    <h3>2 Cancelled</h3>
                 </div>
             </div>
         </div>
-        <div className="clientAppointments"></div>
       </div>
     </main>
   );
