0f470861666ceba3b00f6f7c17fd43643a01202e
Lilianada <lilibest30.lb@gmail.com>
2022-11-30 07:42:56 -0500

    ..

--- src/components/StylistAdmin/AdminClientPage/AdminClientProfile/AppointmentTable.jsx
+++ src/components/StylistAdmin/AdminClientPage/AdminClientProfile/AppointmentTable.jsx
@@ -14,29 +14,57 @@ export default function AppointmentTable({ data }) {
   // }
 
   return (
-    <table className="tableBody">
-      <tr className="tableHead">
-        <th>Date</th>
-        <th>Appointment ID</th>
-        <th>Amount</th>
-        <th>Status</th>
-      </tr>
-      {data.map((val) => {
-        return (
-          <tr key={val.id} className="tableRow">
-            <td className="tableData">{val.date}</td>
-            <td className="tableData">{val.id}</td>
-            <td className="tableData">${val.amount}</td>
-            <td
-              className="completedStatus"
-            >
-              {
-                val.status
-              }
-            </td>
-          </tr>
-        );
-      })}
-    </table>
+    // <table className="tableBody">
+    //   <tr className="tableHead">
+    //     <th>Date</th>
+    //     <th>Appointment ID</th>
+    //     <th>Amount</th>
+    //     <th>Status</th>
+    //   </tr>
+    //   {data.map((val) => {
+    //     return (
+    //       <tr key={val.id} className="tableRow">
+    //         <td className="tableData">{val.date}</td>
+    //         <td className="tableData">{val.id}</td>
+    //         <td className="tableData">${val.amount}</td>
+    //         <td
+    //           className="completedStatus"
+    //         >
+    //           {
+    //             val.status
+    //           }
+    //         </td>
+    //       </tr>
+    //     );
+    //   })}
+    // </table>
+
+    <section className="tableBody">
+        <div className="tableHead">
+            <p>Date</p>
+            <p>Appointment</p>
+            <p>Amount</p>
+            <p>Status</p>
+        </div>
+        <div className="tableItem">
+            {data.map((val) => {
+            return (
+            <div key={val.id} className="tableRow">
+                <p className="tableData">{val.date}</p>
+                <p className="tableData">{val.id}</p>
+                <p className="tableData">${val.amount}</p>
+                <p
+                className="completedStatus"
+                >
+                    <BsCheckCircleFill />
+                {
+                    val.status
+                }
+                </p>
+            </div>
+            );
+        })}
+        </div>
+    </section>
   );
 }
