2bec2738c8095ed513484e919f1e5ec24258c21b
Lilianada <lilibest30.lb@gmail.com>
2022-11-30 04:54:30 -0500

    ...

--- src/components/StylistAdmin/AdminClientPage/AdminClientProfile/AppointmentTable.jsx
+++ src/components/StylistAdmin/AdminClientPage/AdminClientProfile/AppointmentTable.jsx
@@ -1,6 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsCheckCircleFill } from "react-icons/bs";
 import "./AdminClientProfile.scss";
 
-export default function AppointmentTable() {
-  return <div>AppointmentTable</div>;
+export default function AppointmentTable({ data }) {
+  const [completed, setCompleted] = useState(data.status === "Completed");
+  // const isCompleted = () => {
+  //     setCompleted(!completed);
+  //     if (data.status === "Completed"){
+  //         setCompleted(true);
+  //     } else if (data.status === "Cancelled"){
+  //         setCompleted(false)
+  //     }
+  // }
+
+  return (
+    <table className="tableBody">
+      <tr className="tableHead">
+        <th>Date</th>
+        <th>Appointment ID</th>
+        <th>Amount</th>
+        <th>Status</th>
+      </tr>
+      {data.map((val) => {
+        return (
+          <tr key={val.id} className="tableRow">
+            <td className="tableData">{val.date}</td>
+            <td className="tableData">{val.id}</td>
+            <td className="tableData">${val.amount}</td>
+            <td
+              className="completedStatus"
+            >
+              {
+                val.status
+              }
+            </td>
+          </tr>
+        );
+      })}
+    </table>
+  );
 }
--- src/components/StylistAdmin/AdminClientPage/AdminClientProfile/TableData.js
+++ src/components/StylistAdmin/AdminClientPage/AdminClientProfile/TableData.js
@@ -1,50 +1,60 @@
 export const tableData = [
   {
+    date: "22/11/2022",
     name: 'Amancio Ortega',
     id: 620293,
     amount: 180,
     status: "Completed"
   }, {
+    date: "22/11/2022",
     name: 'Bernard Arnault',
     id: 760293,
     amount: 185,
     status: "Completed"
   }, {
+    date: "22/11/2022",
     name: 'Bill Gates',
     id: 960293,
     amount: 280,
     status: "Cancelled"
   }, {
+    date: "22/11/2022",
     name: 'Carlos Sim Helu',
     id: 640293,
     amount: 150,
     status: "Completed"
   }, {
+    date: "22/11/2022",
     name: 'Jeff Bezos',
     id: 1302931,
     amount: 300,
     status: "Completed"
   }, {
+    date: "22/11/2022",
     name: 'Larry Ellison',
     id: 58029,
     amount: 120,
     status: "Cancelled"
   }, {
+    date: "22/11/2022",
     name: 'Larry Page',
     id: 500293.8,
     amount: 180,
     status: "Completed"
   }, {
+    date: "22/11/2022",
     name: 'Mark Zuckerberg',
     id: 620293,
     amount: 180,
     status: "Cancelled"
   }, {
+    date: "22/11/2022",
     name: 'Michael Bloomberg',
     id: 550293,
     amount: 100,
     status: "Completed"
   }, {
+    date: "22/11/2022",
     name: 'Warren Buffet',
     id: 820293,
     amount: 200,
