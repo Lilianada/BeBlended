a2830aa4d4c9fd58fe2dce081f436be173c1f7b4
Lilianada <lilibest30.lb@gmail.com>
2022-12-02 19:31:13 -0500

    ...

--- src/components/ClientAdmin/ClientAdminData.js
+++ src/components/ClientAdmin/ClientAdminData.js
@@ -1,4 +1,4 @@
-export const clientList = [
+export const favouriteList = [
   {
     id: 1,
     clientImage:
@@ -52,88 +52,5 @@ export const clientList = [
     clientImage:
       "https://images.unsplash.com/photo-1662859010103-61b5735968ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMTJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
     clientName: "Adjua James",
-  },
-];
-
-export const tableData = [
-  {
-    date: "22/11/2022",
-    name: "Amancio Ortega",
-    id: 62293,
-    amount: 180,
-    status: "Completed",
-    service: "Classic Box Braids",
-  },
-  {
-    date: "22/11/2022",
-    name: "Bernard Arnault",
-    id: 76023,
-    amount: 185,
-    status: "Completed",
-    service: "Classic Box Braids",
-  },
-  {
-    date: "22/11/2022",
-    name: "Bill Gates",
-    id: 96023,
-    amount: 280,
-    status: "Cancelled",
-    service: "Classic Box Braids",
-  },
-  {
-    date: "22/11/2022",
-    name: "Carlos Sim Helu",
-    id: 64023,
-    amount: 150,
-    status: "Completed",
-    service: "Classic Box Braids",
-  },
-  {
-    date: "22/11/2022",
-    name: "Jeff Bezos",
-    id: 13021,
-    amount: 300,
-    status: "Completed",
-    service: "Classic Box Braids",
-  },
-  {
-    date: "22/11/2022",
-    name: "Larry Ellison",
-    id: 58029,
-    amount: 120,
-    status: "Cancelled",
-    service: "Classic Box Braids",
-  },
-  {
-    date: "22/11/2022",
-    name: "Larry Page",
-    id: 50293,
-    amount: 180,
-    status: "Completed",
-    service: "Classic Box Braids",
-  },
-  {
-    date: "22/11/2022",
-    name: "Mark Zuckerberg",
-    id: 60293,
-    amount: 180,
-    status: "Cancelled",
-    service: "Classic Box Braids",
-  },
-  {
-    date: "22/11/2022",
-    name: "Michael Bloomberg",
-    id: 50993,
-    amount: 100,
-    status: "Completed",
-    service: "Classic Box Braids",
-  },
-  {
-    date: "22/11/2022",
-    name: "Warren Buffet",
-    id: 82093,
-    amount: 200,
-    status: "Completed",
-    service: "Classic Box Braids",
   },
 ];
--- src/components/ClientAdmin/FavouritesPage/FavouritesPage.jsx
+++ src/components/ClientAdmin/FavouritesPage/FavouritesPage.jsx
@@ -1,50 +1,63 @@
 import React from "react";
 import { GrPrevious } from "react-icons/gr";
-import { ClientAdminHeader, BottomNavClient, FavouriteCards } from "../../../components";
-import BeblendedHead from "../../../../assets/Admin-Icons/Beblended-head.svg";
-import './FavouritesPage.scss';
+import {
+  ClientAdminHeader,
+  BottomNavClient,
+  FavouriteCards,
+} from "../../../components";
+import BeblendedHead from "../../../assets/Admin-Icons/Beblended-head.svg";
+import {favouriteList} from "../ClientAdminData";
+import "./FavouritesPage.scss";
 
 export default function FavouritesPage() {
   return (
     <main className="mainWrapper favouritesPage">
-        <ClientAdminHeader/>
+      <ClientAdminHeader />
 
-        <div className="wd_80">
-            <div className="favourites_head">
-                <GrPrevious style={{ marginRight: ".5rem" }} />
-                <p>Back</p>
-            </div> 
-            <div className="favourites_subhead">
-                <h4 className="subhead_text">Favourite Stylists</h4>
-                <p>This is the selection of stylists you like most!</p>
-            </div>
+      <div className="wd_80">
+        <div className="favourites_head">
+          <GrPrevious style={{ marginRight: ".5rem" }} />
+          <p>Back</p>
         </div>
-        <hr />
+        <div className="favourites_subhead">
+          <h4 className="subhead_text">Favourite Stylists</h4>
+          <p>This is the selection of stylists you like most!</p>
+        </div>
+      </div>
+      <hr />
 
-        <div className="favourites_body">
-          {clientList.length === 9 ? (
-            <div className="noFavouritesCard">
-                <img src={BeblendedHead} alt="No Favourite" className="noFavourite_Img" />
-                <p className="noFavourite_Txt">You have no clients in <br/> our system yet!</p>
-                <p className="smallNote">Clients get added once you start <br/> booking appointments.</p>
-            </div>
-          ) : (
-              clientList.map((card, id) => {
-                  return (
-                  <div className="favouritesCards">
+      <div className="favourites_body">
+        {favouriteList.length < 9 ? (
+          <div className="noFavouritesCard">
+            <img
+              src={BeblendedHead}
+              alt="No Favourite"
+              className="noFavourite_Img"
+            />
+            <p className="noFavourite_Txt">
+            You haven’t favourited any<br /> stylists yet!
+            </p>
+            <p className="smallNote">
+            Click the 🖤 on a stylist’s profile to<br/> add them to this list
+            </p>
+          </div>
+        ) : (
+            favouriteList.map((card, id) => {
+            return (
+              <div className="favouritesCards">
                 <FavouriteCards
                   favouritesImage={card.favouritesImage}
                   favouritesName={card.favouritesName}
                   key={id}
-                  />
-              <button className="btn-secondary">View More</button>
-                  </div>
-              );
-            })
-          )}
+                />
+                <button className="btn-secondary">View More</button>
+              </div>
+            );
+          })
+        )}
       </div>
 
-        <BottomNavClient/>
+      <BottomNavClient />
     </main>
   );
 }
