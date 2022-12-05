a96693698e0efc01540b05662aeeb85e01411196
Lilianada <lilibest30.lb@gmail.com>
2022-12-04 23:54:05 -0500

    ...

--- src/components/ClientAdmin/ClientUpcomingBookings/PastUpcomingBookings/UpcomingBookings.jsx
+++ src/components/ClientAdmin/ClientUpcomingBookings/PastUpcomingBookings/UpcomingBookings.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import {
+  
+  ErrorBoundary,
+} from "../../../../components";
+import { IoCalendarSharp } from "react-icons/io5";
+import { bookings } from "../../../StylistBookingPage/CreateAppointmentData";
+import "./UpcomingBookings.scss";
 
 export default function UpcomingBookings() {
-  return <div>UpcomingBookings</div>;
+  <ErrorBoundary>
+  <>
+    {bookings.length > 0 ? (
+      <div className={`mappedTabs ${activeTab === 1 ? "activeTab" : ""}`}>
+        {bookings.map((item, id) => {
+          return <BookingServiceCard item={item} key={id} />;
+        })}
+      </div>
+    ) : (
+      <div
+        className={`noPastBooking ${activeTab === 1 ? "activeTab" : ""}`}
+        onClick={open}
+      >
+        <div className="calendarIcon">
+          <IoCalendarSharp size={40} />
+        </div>
+        <p className="noBookingText">You have no upbookings.</p>
+      </div>
+    )}
+  </>
+</ErrorBoundary>
 }
