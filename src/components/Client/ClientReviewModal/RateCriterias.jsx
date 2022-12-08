6a0c65f19221087af3f48327c6b6fee3df400fab
Lilianada <lilibest30.lb@gmail.com>
2022-12-08 13:29:33 -0500

    rating

--- src/components/Client/ClientReviewModal/RateCriterias.jsx
+++ src/components/Client/ClientReviewModal/RateCriterias.jsx
@@ -1,5 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
+import { Rating } from "react-simple-star-rating";
+
 
 export default function RateCriterias() {
-  return <div>RateCriterias</div>;
+    const [rating, setRating] = useState(0);
+
+  // Catch Rating value
+  const handleRating = (rate) => {
+    setRating(rate);
+  };
+  return (
+    <div className="rateCriterias">
+        <div className="criteria">
+            <p className="boldText">Punctuality</p>
+            <Rating
+              onClick={handleRating}
+              ratingValue={rating}
+              size={24}
+              fillColor="#893583"
+              allowHover={false}
+            />
+        </div>
+        <div className="criteria">
+            <p className="boldText">Communication</p>
+            <Rating
+              onClick={handleRating}
+              ratingValue={rating}
+              size={24}
+              fillColor="#893583"
+              allowHover={false}
+            />
+        </div>
+        <div className="criteria">
+            <p className="boldText">Professionalism</p>
+            <Rating
+              onClick={handleRating}
+              ratingValue={rating}
+              size={24}
+              fillColor="#893583"
+              allowHover={false}
+            />
+        </div>
+        <div className="criteria">
+            <p className="boldText">Hair Preparation</p>
+            <Rating
+              onClick={handleRating}
+              ratingValue={rating}
+              size={24}
+              fillColor="#893583"
+              allowHover={false}
+            />
+        </div>
+    </div>
+  );
 }
