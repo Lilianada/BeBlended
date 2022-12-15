b8529e573eabc0d55d42050b84d0d762eb144aab
Lilianada <lilibest30.lb@gmail.com>
2022-12-15 07:29:23 -0500

    ...

--- src/components/Client/ClientReviewModal/ClientComment.jsx
+++ src/components/Client/ClientReviewModal/ClientComment.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ClientComment() {
+  const [active, setActive] = useState(1);
+  const isActive = (index, e) => {
+    e.preventDefault(e);
+    setActive(state => {
+      return{
+        ...state,
+        [index]: !state[index]
+      }
+    });
+  }
   return (
     <form action="" className="commentSection">
       <p className="boldText">Comments</p>
@@ -12,8 +22,8 @@ export default function ClientComment() {
       ></textarea>
       <p className="boldText">Was Lola Adeyemi a good client? </p>
       <div className="buttonRow">
-        <button className="activeBtn">Yes</button>
-        <button className="inactiveBtn">No</button>
+        <button className={active === 1 ? "inactiveBtn" : "activeBtn" } onClick={() => isActive(1)} >Yes</button>
+        <button className={active === 2 ? "inactiveBtn" : "activeBtn" } onClick={() => isActive(2)} >No</button>
       </div>
       <button className="submitBtn">Submit Review</button>
     </form>
--- src/components/StylistProps/StylistReviewModal/StylistComment.jsx
+++ src/components/StylistProps/StylistReviewModal/StylistComment.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import './StylistReviewModal.scss';
 
 export default function ClientComment() {
+  const [active, setActive] = useState(1);
+  const isActive = (index, e) => {
+    e.preventDefault(e);
+    setActive(state => {
+      return{
+        ...state,
+        [index]: !state[index]
+      }
+    });
+  }
   return (
     <form action="" className="commentSection">
       <p className="boldText">Comments</p>
@@ -10,10 +20,11 @@ export default function ClientComment() {
         className="commentArea"
         cols="30"
         rows="10"
+        placeholder="Good or bad, describe your experience here!"
       ></textarea>
       <p className="boldText">Would you recommend Braids by Becky? </p>
       <div className="buttonRow">
-        <button className="activeBtn">Yes</button>
+        <button className="inactiveBtn">Yes</button>
         <button className="inactiveBtn">No</button>
       </div>
       <button className="submitBtn">Submit Review</button>
