5cc08145c90fc54ce8ca591e145c205e1edb8542
Lilianada <lilibest30.lb@gmail.com>
2022-12-16 21:53:57 -0500

    new files

---
+++ src/components/BeblendedAdmin/DashboardPage/DashboardMenuPages/DashboardModals/PastAppointmentModal/PastAppointmentModal.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PastAppointmentModal() {
+  return <div>PastAppointmentModal</div>;
+}
---
+++ src/components/BeblendedAdmin/DashboardPage/DashboardMenuPages/DashboardModals/UpcomingAppointmentModal/UpcomingAppointmentModal.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function UpcomingAppointmentModal() {
+  return <div>UpcomingAppointmentModal</div>;
+}
