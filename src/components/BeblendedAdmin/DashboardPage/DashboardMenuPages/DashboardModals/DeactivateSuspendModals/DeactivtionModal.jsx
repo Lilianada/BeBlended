ab5deeb7f1f8410b85a45d34fef38a8cf614a80a
Lilianada <lilibest30.lb@gmail.com>
2022-12-16 21:58:22 -0500

    ...

---
+++ src/components/BeblendedAdmin/DashboardPage/DashboardMenuPages/DashboardModals/DeactivateSuspendModals/DeactivtionModal.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DeactivtionModal() {
+  return <div>DeactivtionModal</div>;
+}
---
+++ src/components/BeblendedAdmin/DashboardPage/DashboardMenuPages/DashboardModals/DeactivateSuspendModals/SuspensionModal.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SuspensionModal() {
+  return <div>SuspensionModal</div>;
+}
